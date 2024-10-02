0e18d42ee40e1dc4ea27a4876020ead9adc13aba
Choi-Suhyeon <1AzyF0x@protonmail.ch>
2024-10-03 00:46:58 +0900

    프로세스들의 부모 자식 관계를 구하는 함수 작성

--- so.py
+++ so.py
@@ -1,1 +1,17 @@
-def get_process_tree():
+from functools import reduce
+from process import Proc
+import psutil
+
+def get_process_tree(procs):
+    '''
+    [param]
+    procs: (iterable) process tree를 구할 Proc 객체들의 시퀀스.
+
+    [return]
+    pid를 key로 key의 자식 프로세스들을(Proc 객체들의 집합) value로 하는 딕셔너리.
+    '''
+    return reduce(lambda d, p: d | { p.ppid(): d.get(p.ppid(), set()) | set((p,)) }, procs, dict())
+
+# a = get_process_tree(Proc(i) for i in psutil.process_iter())
+# print(a)
+# print('------\n', a.keys())
